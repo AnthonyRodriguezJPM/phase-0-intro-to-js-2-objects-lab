682f56f25e0cd49f33ad769dc2ecf8af65f1bc9e
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-18 00:37:31 -0500

    commit tree 860b129da7f86e0c90917a81023d7fbec1b3396a

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Write your solution in this file!
+const employee={name: 'Anthony', streetAddress: 'Houston, TX'};
+
+for (const key in employee) {
+    delete employee[key];
+  }
+
+  employee.name = 'Sam';
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newUpdate={...employee}
+    newUpdate[key]=value
+    return newUpdate
+}
+
+console.log(updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway'))
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+for (const key in employee) {
+    delete employee[key];
+  }
+
+  employee.name = 'Sam';
+  employee.streetAddress='11 Broadway'
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+    return employee;
+}
+
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Tony'));
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+for (const key in employee) {
+    delete employee[key];
+  }
+
+  employee.name = 'Sam';
+  employee.streetAddress='11 Broadway'
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee={...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+console.log(deleteFromEmployeeByKey(employee, 'name'));
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+for (const key in employee) {
+    delete employee[key];
+  }
+
+  employee.name = 'Sam';
+  employee.streetAddress='11 Broadway'
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+     delete employee[key];
+     return employee;
+  }
+
+  console.log(destructivelyDeleteFromEmployeeByKey(employee, 'name'));
